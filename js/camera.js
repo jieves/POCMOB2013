385e88ec9f4c1ee630d820d3e48757cd66dcc7f3
Joost Verheyen <jieves@pandora.be>
2013-03-22 16:12:46 +0100

    commit

--- js/camera.js
+++ js/camera.js
@@ -1,113 +1,134 @@
-var pictureSource;   // picture source
-var destinationType; // sets the format of returned value 
-var currentImage; // saves the current image
+// Called when capture operation is finished
+        //
+        function captureSuccess(mediaFiles) {
+            //var i, len;
+            //for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+            //    //uploadFile(mediaFiles[i]);
+            //}
+            //navigator.notification.alert('Weee', null, 'Great success!');
+        }
 
-// Wait for Cordova to connect with the device
-//
-document.addEventListener("deviceready",onDeviceReady,false);
+        // Called if something bad happens.
+        // 
+        function captureError(error) {
+            //var msg = 'An error occurred during capture: ' + error.code;
+            //navigator.notification.alert(msg, null, 'Uh oh!');
+        }
 
-// Cordova is ready to be used!
-//
-function onDeviceReady() {
-    pictureSource=navigator.camera.PictureSourceType;
-    destinationType=navigator.camera.DestinationType;
-}
+        // A button will call this function
+        //
+        function captureImage() {
+            // Launch device camera application, 
+            // allowing user to capture up to 2 images
+            navigator.device.capture.captureImage(captureSuccess, captureError, { limit: 2 });
+        }
 
-// Called when a photo is successfully retrieved
-//
-function onPhotoDataSuccess(imageData) {
-  // Uncomment to view the base64 encoded image data
-  // console.log(imageData);
+        // Upload files to server
+        function uploadFile(mediaFile) {
+            var ft = new FileTransfer(),
+                path = mediaFile.fullPath,
+                name = mediaFile.name;
 
-  // Get image handle
-  //
-  var smallImage = document.getElementById(currentImage);
+            ft.upload(path,
+                "http://my.domain.com/upload.php",
+                function (result) {
+                    console.log('Upload success: ' + result.responseCode);
+                    console.log(result.bytesSent + ' bytes sent');
+                },
+                function (error) {
+                    console.log('Error uploading file ' + path + ': ' + error.code);
+                },
+                { fileName: name });
+        }
 
-  // Unhide image elements
-  //
-  smallImage.style.display = 'block';
+        var pictureSource;   // picture source
+        var destinationType; // sets the format of returned value 
 
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  //
-  smallImage.src = "data:image/jpeg;base64," + imageData;
-}
+        // Wait for Cordova to connect with the device
+        //
+        function onLoad() { document.addEventListener("deviceready", onDeviceReady, false); }
 
-// A button / link will call this function
-//
-function capturePhoto(imageId) {
-  // Take picture using device camera and retrieve image as base64-encoded string
-  currentImage = imageId;
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-    destinationType: destinationType.DATA_URL, targetWidth: 800, targetHeight: 800 });
-}
+        // Cordova is ready to be used!
+        //
+        function onDeviceReady() {
+            pictureSource = navigator.camera.PictureSourceType;
+            destinationType = navigator.camera.DestinationType;
+            alert("device is ready");
+        }
 
-//Called if something bad happens.
-//
-function onFail(message) {
-alert('Failed because: ' + message);
-}
+        // Called when a photo is successfully retrieved
+        //
+        function onPhotoDataSuccess(imageData) {
+            // Uncomment to view the base64 encoded image data
+            // console.log(imageData);
 
+            // Get image handle
+            //
+            var smallImage = document.getElementById('smallImage');
 
+            // Unhide image elements
+            //
+            smallImage.style.display = 'block';
 
-// Called when a photo is successfully retrieved
-//
-function onPhotoURISuccess(imageURI) {
-  // Uncomment to view the image file URI 
-  // console.log(imageURI);
+            // Show the captured photo
+            // The inline CSS rules are used to resize the image
+            //
+            smallImage.src = "data:image/jpeg;base64," + imageData;
+        }
 
-  // Get image handle
-  //
-  var largeImage = document.getElementById('largeImage');
+        // Called when a photo is successfully retrieved
+        //
+        function onPhotoURISuccess(imageURI) {
+            // Uncomment to view the image file URI 
+            // console.log(imageURI);
 
-  // Unhide image elements
-  //
-  largeImage.style.display = 'block';
+            // Get image handle
+            //
+            var largeImage = document.getElementById('largeImage');
 
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  //
-  largeImage.src = imageURI;
-}
+            // Unhide image elements
+            //
+            largeImage.style.display = 'block';
 
-// A button will call this function
-//
-function capturePhotoEdit() {
-  // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
-    destinationType: destinationType.DATA_URL });
-}
+            // Show the captured photo
+            // The inline CSS rules are used to resize the image
+            //
+            largeImage.src = imageURI;
+        }
 
-// A button will call this function
-//
-function getPhoto(source) {
-  // Retrieve image file location from specified source
-  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
-    destinationType: destinationType.FILE_URI,
-    sourceType: source });
-}
+        // A button will call this function
+        //
+        function capturePhoto() {
+            // Take picture using device camera and retrieve image as base64-encoded string
+            navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+                quality: 50,
+                destinationType: destinationType.DATA_URL
+            });
+        }
 
-/*
-function onPhotoDataSuccess(imageData) 
-{
-     //I want to save my image here in sdcard folder.**
-    var nodeid = localStorage.getItem("user_nodeid");
-    var modifyImgData = imageData.replace(' ', '+');
-    document.getElementById('image').src = setLocalStorageImage(localStorage.getItem(nodeid+"image"), modifyImgData);
-    document.getElementById('profileMenu').src = setLocalStorageImage(localStorage.getItem(nodeid+"smallimage"), modifyImgData);
-    $.ajax({
-           type: "POST",
-           url: appURL+"api/upload/image/" +nodeid+ "/1",
-           data: "image=" + encodeURIComponent(modifyImgData),
-           success: function(msg){
-                    //No need to do anything here
-                    if (msg.documentElement.getElementsByTagName("message")[0].childNodes[0].nodeValue != 'success')
-                        onFail('Error in uploading image at server. Please try again.');
-           }
-        });
-}
-*/
+        // A button will call this function
+        //
+        function capturePhotoEdit() {
+            // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
+            navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+                quality: 20, allowEdit: true,
+                destinationType: destinationType.DATA_URL
+            });
+        }
 
-function onFail(message){
-    alert(message);
-}
+        // A button will call this function
+        //
+        function getPhoto(source) {
+            // Retrieve image file location from specified source
+            navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+                quality: 50,
+                destinationType: destinationType.FILE_URI,
+                sourceType: source
+            });
+        }
+
+        // Called if something bad happens.
+        // 
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
