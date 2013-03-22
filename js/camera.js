3f783e9f3e4b6e83b54902be214af6f9a711387c
Joost Verheyen <jieves@pandora.be>
2013-03-22 16:00:07 +0100

    commit

--- js/camera.js
+++ js/camera.js
@@ -13,38 +13,101 @@ function onDeviceReady() {
     destinationType=navigator.camera.DestinationType;
 }
 
-function capturePhoto(){     
-  navigator.camera.getPicture(uploadPhoto,null,{sourceType:1,quality:60}); 
-} 
- 	  
-function uploadPhoto(data){ 
-   // this is where you would send the image file to server 
-   //output image to screen     
-   cameraPic.src = "data:image/jpeg;base64," + data; 
-} 
-	  
-function uploadPhoto(data){ 
-  // this is where you would send the image file to server     
-  cameraPic.src = "data:image/jpeg;base64," + data;     
-  // Successful upload to the server     
-  navigator.notification.alert(         
-		    'Your Photo has been uploaded',   // message         
-			okay,                             // callback         
-			'Photo Uploaded',              	  // title         
-			'OK'                             // buttonName     
-			);     
-		  // upload has failed Fail     
-		  /*     if (failedToUpload){     
-		       navigator.notification.alert(         
-			   'Your Photo has failed to upload',         
-			   failedDismissed,         
-			   'Photo Not Uploaded',         
-			   'OK'         
-			   );     
-			   }     
-			   */ 
-} 
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+  // Uncomment to view the base64 encoded image data
+  // console.log(imageData);
 
-function okay(){     
-  // Do something 
-} 
+  // Get image handle
+  //
+  var smallImage = document.getElementById(currentImage);
+
+  // Unhide image elements
+  //
+  smallImage.style.display = 'block';
+
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  //
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+// A button / link will call this function
+//
+function capturePhoto(imageId) {
+  // Take picture using device camera and retrieve image as base64-encoded string
+  currentImage = imageId;
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+    destinationType: destinationType.DATA_URL, targetWidth: 800, targetHeight: 800 });
+}
+
+//Called if something bad happens.
+//
+function onFail(message) {
+alert('Failed because: ' + message);
+}
+
+
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoURISuccess(imageURI) {
+  // Uncomment to view the image file URI 
+  // console.log(imageURI);
+
+  // Get image handle
+  //
+  var largeImage = document.getElementById('largeImage');
+
+  // Unhide image elements
+  //
+  largeImage.style.display = 'block';
+
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  //
+  largeImage.src = imageURI;
+}
+
+// A button will call this function
+//
+function capturePhotoEdit() {
+  // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+    destinationType: destinationType.DATA_URL });
+}
+
+// A button will call this function
+//
+function getPhoto(source) {
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+    destinationType: destinationType.FILE_URI,
+    sourceType: source });
+}
+
+/*
+function onPhotoDataSuccess(imageData) 
+{
+     //I want to save my image here in sdcard folder.**
+    var nodeid = localStorage.getItem("user_nodeid");
+    var modifyImgData = imageData.replace(' ', '+');
+    document.getElementById('image').src = setLocalStorageImage(localStorage.getItem(nodeid+"image"), modifyImgData);
+    document.getElementById('profileMenu').src = setLocalStorageImage(localStorage.getItem(nodeid+"smallimage"), modifyImgData);
+    $.ajax({
+           type: "POST",
+           url: appURL+"api/upload/image/" +nodeid+ "/1",
+           data: "image=" + encodeURIComponent(modifyImgData),
+           success: function(msg){
+                    //No need to do anything here
+                    if (msg.documentElement.getElementsByTagName("message")[0].childNodes[0].nodeValue != 'success')
+                        onFail('Error in uploading image at server. Please try again.');
+           }
+        });
+}
+*/
+
+function onFail(message){
+    alert(message);
+}
